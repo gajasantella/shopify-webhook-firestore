dadd56358907aafd89a726acb42328e51355b7f8
gajasantella <gaiasantella54@gmail.com>
2025-03-06 19:30:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
 app.post('/shopify-webhook-order', (req, res) => {
   console.log('Webhook ricevuto:', req.body);
-  res.status(200).json({message: "Ricevuto", body: req.body});
+  res.status(200).send('OK');
 });
+
+app.listen(process.env.PORT || 3000);
