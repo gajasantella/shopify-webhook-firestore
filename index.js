f10499ce68f5337bb4d2f70055c6b555b3cbd6f1
gajasantella <gaiasantella54@gmail.com>
2025-03-06 19:24:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
-const express = require('express');
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-admin.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-const firestore = admin.firestore();
-const app = express();
-app.use(express.json());
-
-app.post('/shopify-webhook-order', async (req, res) => {
-  const order = req.body;
-
-  await firestore.collection('orders').doc(`${order.id}`).set({
-    orderId: order.id,
-    orderStatus: 'pending',
-    customerEmail: order.email,
-    requirementsReceived: false,
-    createdAt: admin.firestore.FieldValue.serverTimestamp(),
-    updatedAt: admin.firestore.FieldValue.serverTimestamp(),
-    deliveredFiles: []
-  });
-
-  res.status(200).send('Webhook ricevuto!');
-});
-
-app.get('/', (req,res)=> {
-  res.send('Webhook Shopify pronto!');
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Webhook Shopify attivo');
+app.post('/shopify-webhook-order', (req, res) => {
+  console.log('Webhook ricevuto:', req.body);
+  res.status(200).json({message: "Ricevuto", body: req.body});
 });
